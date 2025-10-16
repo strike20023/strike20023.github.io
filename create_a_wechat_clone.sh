753fcedffba6357830ff32ff1bb19ea5bb674ea6
Herman Huang <35003938+strike20023@users.noreply.github.com>
2025-10-16 18:18:16 +0800

    Update WeChat installation script with improvements

--- create_a_wechat_clone.sh
+++ create_a_wechat_clone.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 set -e
 URL="https://dldir1v6.qq.com/weixin/Universal/Mac/WeChatMac.dmg"
@@ -7,19 +6,23 @@ MNT="/Volumes/微信 WeChat"
 APP="/Applications/WeChat.app"
 APP2="/Applications/微信_副本.app"
 ICON_URL="https://strike20023.github.io/wechat_blue_icon.icns"
+
 sudo ls
 pkill -9 WeChat || true
 [ -f "$DMG" ] && rm -f "$DMG"
+
 curl -L -o "$DMG" "$URL"
 hdiutil detach "$MNT" -force || true
 sleep 1
 hdiutil attach "$DMG" -nobrowse
+
 [ -d "$APP" ] && rm -rf "$APP"
+[ -d "$APP2" ] && rm -rf "$APP2"
 cp -R "$MNT/WeChat.app" /Applications/
+cp -R "$MNT/WeChat.app" "$APP2"
+
 hdiutil detach "$MNT" -force || true
 rm -f "$DMG"
-
-cp -r $APP $APP2
 
 sudo curl -L -o $APP2/Contents/Resources/AppIcon.icns "$ICON_URL"
 sudo plutil -replace CFBundleIdentifier -string "com.tencent.WeiXin" $APP2/Contents/Info.plist
