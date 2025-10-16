eb34506a4dbb1e73b559e4ad283014a3c2e1e858
Herman Huang <35003938+strike20023@users.noreply.github.com>
2025-10-16 18:55:12 +0800

    Update WeChat clone script to use sudo for commands

--- create_a_wechat_clone.sh
+++ create_a_wechat_clone.sh
@@ -7,22 +7,22 @@ APP="/Applications/WeChat.app"
 APP2="/Applications/微信_副本.app"
 ICON_URL="https://strike20023.github.io/wechat_blue_icon.icns"
 
-sudo ls
+sudo echo "start"
 pkill -9 WeChat || true
-[ -f "$DMG" ] && rm -f "$DMG"
+[ -f "$DMG" ] && sudo rm -f "$DMG"
 
 curl -L -o "$DMG" "$URL"
 hdiutil detach "$MNT" -force || true
 sleep 1
 hdiutil attach "$DMG" -nobrowse
 
-[ -d "$APP" ] && rm -rf "$APP"
-[ -d "$APP2" ] && rm -rf "$APP2"
+[ -d "$APP" ] && sudo rm -rf "$APP"
+[ -d "$APP2" ] && sudo rm -rf "$APP2"
 cp -R "$MNT/WeChat.app" /Applications/
 cp -R "$MNT/WeChat.app" "$APP2"
 
 hdiutil detach "$MNT" -force || true
-rm -f "$DMG"
+sudo rm -f "$DMG"
 
 sudo curl -L -o $APP2/Contents/Resources/AppIcon.icns "$ICON_URL"
 sudo plutil -replace CFBundleIdentifier -string "com.tencent.WeiXin" $APP2/Contents/Info.plist
